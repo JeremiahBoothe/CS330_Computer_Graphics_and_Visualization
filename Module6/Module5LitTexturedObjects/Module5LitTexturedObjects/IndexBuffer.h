a30ab9269e74dd0e127bf7c7932fd7bf20b688fb
jerem <jeremiahboothe@outlook.com>
2026-01-07 12:52:24 -0500

    Lighting and Texturing Objects in OpenGL

---
+++ Module6/Module5LitTexturedObjects/Module5LitTexturedObjects/IndexBuffer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "ShapeObject.h"
+
+/* Still haven't transfered over to using this */
+class IndexBuffer
+{
+	unsigned int m_RendererID;
+
+	public:
+		IndexBuffer(const void* data, unsigned int size);
+
+		~IndexBuffer();
+
+		void Bind();
+		void Unbind();
+};
